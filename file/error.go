d43a2540cf3367df75f3e293090ee78b8a5671d3
Sasha Koshka <sashakoshka@tebibyte.media>
2022-08-10 14:08:04 -0400

    Fixed error formatting column indicator

--- file/error.go
+++ file/error.go
@@ -58,7 +58,7 @@ func (err Error) Error () (formattedMessage string) {
 
 		// print an arrow with a tail spanning the width of the mistake
 		columnCountdown := err.Location.column
-		for columnCountdown > 0 {
+		for columnCountdown > 1 {
 			formattedMessage += " "
 			columnCountdown --
 		}
@@ -66,9 +66,9 @@ func (err Error) Error () (formattedMessage string) {
 			// TODO: for tabs, print out 8 of these instead.
 			formattedMessage += "-"
 		}
-		formattedMessage += "-\n"
+		formattedMessage += "^\n"
 	}
-	formattedMessage += err.message + "-\n"
+	formattedMessage += err.message + "\n"
 
 	return
 }
