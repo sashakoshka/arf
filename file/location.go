568134a7e9c53f7fd6c284a6180aa4e28df65d04
Sasha Koshka <supergriffin64@gmail.com>
2022-08-18 00:42:47 -0400

    Add getter functions to Location

--- file/location.go
+++ file/location.go
@@ -13,3 +13,26 @@ type Location struct {
 func (location Location) NewError (message string, kind ErrorKind) (err Error) {
 	return NewError(location, message, kind)
 }
+
+// File returns the file the location is in
+func (location Location) File () (file *File) {
+	return location.file
+}
+
+// Row returns the row the location is positioned at in the file, starting at
+// zero.
+func (location Location) Row () (row int) {
+	return location.row
+}
+
+// Column returns the column the location is positioned at in the file, starting
+// at zero.
+func (location Location) Column () (column int) {
+	return location.column
+}
+
+// Width returns the amount of runes spanned by the location, starting at row
+// and column.
+func (location Location) Width () (width int) {
+	return location.width
+}
