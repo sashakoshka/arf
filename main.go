05954546eb337a793da9e23f86bdbd6702767873
Sasha Koshka <sashakoshka@tebibyte.media>
2022-08-30 01:11:37 -0400

    Remove useless files

--- main.go
+++
@@ -1,9 +0,0 @@
-package arf
-
-import "io"
-import "git.tebibyte.media/arf/arf/parser"
-
-func CompileModule (modulePath string, output io.Writer) (err error) {
-	_, err = parser.Parse(modulePath)
-	return
-}
