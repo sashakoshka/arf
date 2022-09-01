f585e5119f3bc91b7e950f0c7176ce70fb1231eb
Sasha Koshka <sashakoshka@tebibyte.media>
2022-09-01 18:03:01 -0400

    Removed useless logging in objt sections

--- parser/objt.go
+++ parser/objt.go
@@ -94,8 +94,6 @@ func (parser *ParsingOperation) parseObjtMember () (
 	member.what, err = parser.parseType()
 	if err != nil { return }
 
-	println(parser.token.Describe())
-	
 	// if there is a bit width, get it
 	if parser.token.Is(lexer.TokenKindBinaryAnd) {
 		err = parser.nextToken(lexer.TokenKindUInt)
