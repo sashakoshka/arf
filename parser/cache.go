a791063d5b817787802314b1732e376ae6657f29
Sasha Koshka <sashakoshka@tebibyte.media>
2022-09-05 13:23:14 -0400

    Add a parser cache map

---
+++ parser/cache.go
@@ -0,0 +1,11 @@
+package parser
+
+// cacheItem stores an item of the parser cache.
+type cacheItem struct {
+	section Section
+	skimmed bool
+}
+
+// cache stores all modules that have been parsed so far. They are indexed with
+// their full path on the filesystem, starting with '/'.
+var cache = make(map[string] cacheItem)
