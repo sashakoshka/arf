0af08a4d2440151bf762863131c8a714a95edc0c
Sasha Koshka <sashakoshka@tebibyte.media>
2022-09-11 15:35:07 -0400

    Merged objt and type test cases

--- parser/type_test.go
+++ parser/type_test.go
@@ -6,10 +6,34 @@ func TestType (test *testing.T) {
 	checkTree ("../tests/parser/type", false,
 `:arf
 ---
-type ro Basic:Int
-type ro BasicInit:Int 6
-type ro IntArray:{Int ..}
-type ro IntArrayInit:Int:3
+type ro aBasic:Obj
+	ro that:Int
+	ro this:Int
+type ro bBitFields:Obj
+	ro that:Int & 1
+	ro this:Int & 24 298
+type ro cInit:Obj
+	ro that:String "hello world"
+	ro this:Int 23
+type ro dInitInherit:aBasic
+	-- that 9384
+	-- this 389
+type ro eInitAndDefine:aBasic
+	-- this 389
+	ro these:aBasic
+		ro born:Int 4
+		ro in:Int
+		ro the:Int:3
+			9348
+			92384
+			92834
+		-- this 98
+	-- that 9384
+
+type ro fBasic:Int
+type ro gBasicInit:Int 6
+type ro hIntArray:{Int ..}
+type ro iIntArrayInit:Int:3
 	3298
 	923
 	92
