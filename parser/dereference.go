fae8bedfa9aca57204b40e46885a764e7637c2d8
Sasha Koshka <sashakoshka@tebibyte.media>
2022-10-11 11:31:44 -0400

    Dereference parsing stub

---
+++ parser/dereference.go
@@ -0,0 +1,12 @@
+package parser
+
+// import "git.tebibyte.media/arf/arf/lexer"
+// import "git.tebibyte.media/arf/arf/infoerr"
+
+func (parser *ParsingOperation) parseDereference () (
+	dereference Dereference,
+	err error,
+) {
+	// TODO
+	return
+}
