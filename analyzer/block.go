552099407229527e88f4ff67b0ec0e1d736e6af0
Sasha Koshka <sashakoshka@tebibyte.media>
2022-10-19 22:46:20 -0400

    Added some basic structs and interfaces for phrase parsing

---
+++ analyzer/block.go
@@ -0,0 +1,5 @@
+package analyzer
+
+type Block struct {
+	locatable
+}
--- analyzer/phrase.go
+++ analyzer/phrase.go
@@ -1,9 +1,16 @@
 package analyzer
 
+import "git.tebibyte.media/arf/arf/file"
 import "git.tebibyte.media/arf/arf/parser"
+import "git.tebibyte.media/arf/arf/infoerr"
 
 type Phrase interface {
-	
+	// Provided by phraseBase
+	Location () (location file.Location)
+	NewError (message string, kind infoerr.ErrorKind) (err error)
+
+	// Must be implemented by each individual phrase
+	ToString (indent int) (output string)
 }
 
 type ArbitraryPhrase struct {
